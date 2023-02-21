8c853b19100610fcb001c9b44771510ac431a72c
Kyle Huffling <kyle@Kyles-MacBook-Air.local>
2023-02-21 16:58:41 -0500

    Menu/PDF

--- components/Mobile-menu.js
+++ components/Mobile-menu.js
@@ -1,8 +1,13 @@
+const openMenu = () => {
+  var element = document.getElementById("mobile-btn");
+  element.classList.toggle("open");
+}
+
 export default function Mobile_Menu() {
   return (
     <div className="mobile-menu bg_white pc_box_shadow">
       <div id="mobile-btn">
-        <div id="nav-icon" className="btn-spans nav-icon" onclick="openMenu()">
+        <div id="nav-icon" className="btn-spans nav-icon" onclick={openMenu}>
           <span></span>
           <span></span>
           <span></span>
@@ -11,9 +16,4 @@ export default function Mobile_Menu() {
       </div>
     </div>
   )
-}
-
-function openMenu() {
-  var element = document.getElementById("mobile-btn");
-  element.classList.toggle("open");
 }
